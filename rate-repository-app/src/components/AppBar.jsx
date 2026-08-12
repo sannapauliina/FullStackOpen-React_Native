9f25b9e53ad26f56576b0c819395ba95c3120c80
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-08-12 16:27:20 +0300

    Tehtävä 10.7: Lisätty vaakasuuntainen ScrollView AppBariin (vieritettävät välilehdet)

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Pressable } from "react-native";
+import { View, StyleSheet, Pressable, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import { Link } from "react-router-native";
 import Text from "./Text";
@@ -8,30 +8,38 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: "#24292e",
     padding: 15,
+  },
+  scroll: {
     flexDirection: "row",
-    gap: 20,
   },
   tab: {
     color: "white",
     fontSize: 18,
     fontWeight: "bold",
+    marginRight: 20,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
-        <Link to="/">
-          <Text style={styles.tab}>Repositories</Text>
-        </Link>
-      </Pressable>
+      <ScrollView
+        horizontal
+        style={styles.scroll}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Pressable>
+          <Link to="/">
+            <Text style={styles.tab}>Repositories</Text>
+          </Link>
+        </Pressable>
 
-      <Pressable>
-        <Link to="/signin">
-          <Text style={styles.tab}>Sign in</Text>
-        </Link>
-      </Pressable>
+        <Pressable>
+          <Link to="/signin">
+            <Text style={styles.tab}>Sign in</Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };
