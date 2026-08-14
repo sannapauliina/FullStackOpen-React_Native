500655174e6fefe055e6f87c85a97da7f001db0b
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-08-14 16:47:23 +0300

    Tehtävä 10.12: Lisätty EXPO_PUBLIC_APOLLO_URI ja siirretty Apollo Clientin osoite .env‑tiedostoon

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    uri: process.env.EXPO_PUBLIC_APOLLO_URI,
     cache: new InMemoryCache(),
   });
 };
