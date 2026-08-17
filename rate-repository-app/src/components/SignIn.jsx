6df3548a222904d4095c25e797950fc62a413bb3
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-08-17 17:29:15 +0300

    Tehtävä 10.13: Toteutettu authenticate‑mutaatio ja lisätty useSignIn‑hook (kirjautuminen)

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -2,6 +2,7 @@ import { View, TextInput, Pressable, StyleSheet } from "react-native";
 import Text from "./Text";
 import { Formik } from "formik";
 import * as yup from "yup";
+import useSignIn from "../hooks/useSignIn";
 
 const styles = StyleSheet.create({
   container: {
@@ -42,8 +43,17 @@ const validationSchema = yup.object().shape({
 });
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
